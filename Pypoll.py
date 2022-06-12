02622f3bafdfe2ff4bbb85e061738298c1421d71
Izzy Irazoque <izzyirazoque@gmail.com>
2022-06-11 17:56:08 -0700

    Adding election audit code

--- Pypoll.py
+++ Pypoll.py
@@ -8,11 +8,25 @@
 # Add our dependencies.
 import csv
 import os
-from sqlite3 import Row
+
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+#voter counter variable
+total_votes = 0
+
+#candidate options
+candidate_options = []
+
+#declare an empty dic
+candidate_votes = {}
+
+#Winning Candidate and Wining Count
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -21,4 +35,42 @@ with open(file_to_load) as election_data:
 
     # Read and print the header row.
     headers = next(file_reader)
-    print(headers)
+    
+    for row in file_reader:
+        total_votes +=1
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+
+            #create each candidate as a KEY
+            candidate_votes[candidate_name] = 0
+            #add a vote to each candidated count
+        candidate_votes[candidate_name] += 1
+        
+        
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+# Winning Candidate and Winning Count Tracker
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+            
+
+
+winning_candidate_summary = (
+    f"---------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:.1f}%\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"---------------------------\n")
+
+
+print(winning_candidate_summary)
